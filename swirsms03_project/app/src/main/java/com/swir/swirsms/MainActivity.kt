56ed0af9cf369fd194121d6d586f14658c4c356f
Wojciech <swirjestem@hotmail.com>
2026-09-26 00:44:33 +0200

    Add SwirSMS MainActivity

---
+++ swirsms03_project/app/src/main/java/com/swir/swirsms/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.swir.swirsms
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.fragment.app.FragmentActivity
+
+class MainActivity : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { SwirSmsApp() }
+    }
+}
